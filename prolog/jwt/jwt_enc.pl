895ffc3bcc70269b650c6960f422d2d4a5e5b8a5
Wouter Beek <me@wouterbeek.com>
2015-06-22 23:00:14 +0200

    ADDED: Automatically set the `typ` header claim and the `iss` and `iat` payload claims when encoding JWTs.

--- prolog/jwt/jwt_enc.pl
+++ prolog/jwt/jwt_enc.pl
@@ -3,7 +3,7 @@
   [
     jwt_enc/4 % +Header:dict
               % +Payload:dict
-              % +Key:dict
+              % ?Key
               % -Token:atom
   ]
 ).
@@ -16,31 +16,58 @@
 
 :- use_module(library(apply)).
 :- use_module(library(base64)).
+:- use_module(library(jwt/jwt_util)).
 :- use_module(library(sha)).
 
-:- use_module(jwt_util).
 
 
 
-%! jwt_enc(+Header:dict, +Payload:dict, +Key:dict, -Token:atom) is det.
 
-jwt_enc(Header, Payload, Key, Token):-
+%! jwt_enc(+Header:dict, +Payload:dict, ?Key, -Token:atom) is det.
+
+jwt_enc(Header0, Payload0, Key, Token):-
+  header_claims(Header0, Header),
+  payload_claims(Payload0, Payload),
   maplist(atom_json_dict, [HeaderDec,PayloadDec], [Header,Payload]),
   maplist(base64url, [HeaderDec,PayloadDec], [HeaderEnc,PayloadEnc]),
   atomic_list_concat([HeaderEnc,PayloadEnc], '.', SignWith),
-  create_signature(Header.alg, SignWith, Key, SignatureEnc),
+  create_signature(Header, SignWith, Key, SignatureEnc),
   atomic_list_concat([SignWith,SignatureEnc], '.', Token).
 
 %! create_signature(
-%!   +Algorithm:string,
+%!   +Header:dict,
 %!   +SignWith:atom,
-%!   +Key:dict,
+%!   ?Key,
 %!   -Signature:atom
 %! ) is det.
 
-create_signature("none", _, _, ''):- !.
-create_signature("HS256", SignWith, Key, SignatureEnc):- !,
-  interpret_key(Key, Secret),
-  hmac_sha(Secret, SignWith, Hash, [algorithm(sha256)]),
+create_signature(Header, _, _, ''):-
+  Header.alg == "none", !.
+create_signature(Header, SignWith, Key, SignatureEnc):- !,
+  hmac_algorithm(Header.alg, Alg), !,
+  interpret_key(Header, Key, Secret),
+  hmac_sha(Secret, SignWith, Hash, [algorithm(Alg)]),
   atom_codes(SignatureDec, Hash),
   base64url(SignatureDec, SignatureEnc).
+
+header_claims(D0, D):-
+  dict_pairs(D0, Tag, L0),
+  (   memberchk(typ-_, L0)
+  ->  L = L0
+  ;   L = [typ-"JWT"|L0]
+  ),
+  dict_pairs(D, Tag, L).
+
+payload_claims(D0, D):-
+  dict_pairs(D0, Tag, L0),
+  (   memberchk(iss-_, L0)
+  ->  L1 = L0
+  ;   L1 = [iss-"SWI-Prolog"|L0]
+  ),
+  (   memberchk(iat-_, L1)
+  ->  L = L1
+  ;   get_time(Now0),
+      Now is floor(Now0),
+      L = [iat-Now|L1]
+  ),
+  dict_pairs(D, Tag, L).
